7e291619dd78ed3c01e94731ccc1a71f36e2e0c1
Vladimir <Sencor@bk.ru>
2016-04-07 19:02:19 +0300

    Dellet Callibration

--- Source/Capture_Video.cpp
+++ Source/Capture_Video.cpp
@@ -1,103 +1,189 @@
-﻿#include <cv.h>
-#include <highgui.h>
-#include <stdlib.h>
+﻿#include <iostream>
+#include "cv.h"
+#include "highgui.h"
+#include "cvaux.h"
 #include <opencv2\calib3d\calib3d_c.h>
-#include <stdio.h>
+#include<opencv2\videoio.hpp>
+#include <iostream>
+#include <vector>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+void stereoGrabberInitFrames();
+void stereoGrabberGrabFrames();
+void stereoGrabberStopCam();
+void SCalibration();
+
+IplImage* imageLeft;
+IplImage* imageRight;
+
+CvCapture *camera1 = NULL, *camera2 = NULL;
+
 
 int main(int argc, char **argv) {
-	IplImage *Limage = 0, *Rimage = 0, *Lgray = 0, *Rgray = 0, *dst=0;
-	dst = cvLoadImage("IMG_2746.JPG", 1);
-	if (dst == NULL)
-	{
-		fprintf(stderr, "ERROR: Could not load left image %s\n");
-		
-		return 1;
-	}
-	Limage = cvCreateImage(cvSize(dst->width /7, dst->height / 7), IPL_DEPTH_8U, dst->nChannels);
-	cvResize(dst, Limage, 1);
-	dst = cvLoadImage("IMG_2747.JPG", 1);
-	if (dst == NULL)
-	{
-		fprintf(stderr, "ERROR: Could not load right image %s\n");
-		return 1;
-	}
-	Rimage = cvCreateImage(cvSize(dst->width / 7, dst->height / 7), IPL_DEPTH_8U, dst->nChannels);
-	cvResize(dst, Rimage, 1);
-	Lgray = cvCreateImage(cvSize(Limage->width, Limage->height), IPL_DEPTH_8U, 1);
-	Rgray = cvCreateImage(cvSize(Rimage->width , Rimage->height), IPL_DEPTH_8U, 1);
-	cvConvertImage(Limage, Lgray, CV_8S);
-	cvConvertImage(Rimage, Rgray, CV_8S);
-	cvFlip(Lgray, 0);
-	cvFlip(Rgray, 0);
-
-	CvMat *Stereo = cvCreateMat(Rgray->height, Rgray->width, CV_16S);
-	CvMat *VStereo = cvCreateMat(Rgray->height, Rgray->width, CV_8U);
 	
-	if (Stereo == NULL)
-	{
-		fprintf(stderr, "ERROR: Could not create depth map %s\n");
-		return 1;
-	}
-	
-	CvStereoBMState* state = cvCreateStereoBMState(CV_STEREO_BM_BASIC,0);
-	if (state == NULL)
-	{
-		fprintf(stderr, "ERROR: Could not create CvStereoBMState\n");
-		return 1;
-	}
-	cvNamedWindow("cvHoughCircles", CV_WINDOW_AUTOSIZE);
-
-
-
-		state->preFilterSize =21;
-		state->preFilterCap = 63;
-		state->SADWindowSize =41;
-		state->minDisparity = 0;
-		state->numberOfDisparities =80;
-		state->textureThreshold =20;
-		state->uniquenessRatio =8;
-		state->speckleWindowSize = 20;
-		state->speckleRange = 15;
-		cvFindStereoCorrespondenceBM(Lgray, Rgray, Stereo, state);
-		cvNormalize(Stereo, VStereo, 0, 256, CV_MINMAX, NULL);
-	CvMemStorage* storage = cvCreateMemStorage(0); //хранилище кругов
-	cvSmooth(VStereo, VStereo, CV_GAUSSIAN, 1, 1);//сглаживание изображения
-	cvNamedWindow("left1", CV_WINDOW_AUTOSIZE);
-	cvShowImage("left1", Lgray);
-	CvSeq* results = cvHoughCircles(
-		Lgray,
-		storage,
-		CV_HOUGH_GRADIENT,
-		5,
-		Lgray->width / 2,
-		100,
-		100,
-	    20,
-		60
-		);//поиск окружностей
-	CvScalar s;
-	s.val[0] = 0;
-	for (int i = 0; i < results->total; i++) {
-		float* p = (float*)cvGetSeqElem(results, i);
-		printf("%f%f%f\n", p[0], p[1], p[2]);
-		CvPoint pt = cvPoint(cvRound(p[0]), cvRound(p[1]));
-		cvCircle(Lgray, pt, cvRound(p[2]), CV_RGB(0xff,0, 0xff),4);
-		s = cvGet2D(VStereo, p[1], p[0]);
-		printf("%f\n", s.val[0]);
-	}//прорисовка окружностей
-	cvNamedWindow("cvHoughCircles", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("left", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("right", CV_WINDOW_AUTOSIZE);
-	cvShowImage("left", Lgray);
-	cvShowImage("right", Rgray);
-	cvShowImage("cvHoughCircles", VStereo);
-	cvWaitKey(0);
-	cvReleaseImage(&Limage);
-	cvReleaseImage(&Rimage);
-	cvReleaseImage(&Lgray);
-	cvReleaseImage(&Rgray);
-	cvReleaseMat(&VStereo);
-	cvReleaseMat(&Stereo);
-	cvDestroyAllWindows();
 	return 0;
+}
+
+
+void  stereoGrabberInitFrames()
+{
+	camera1 = cvCaptureFromCAM(2);
+	assert(camera1 != NULL);
+	cvWaitKey(100);
+
+	camera2 = cvCaptureFromCAM(1);
+	assert(camera2 != NULL); // Inicializaciya kamer
+}
+
+void stereoGrabberGrabFrames()
+{
+	imageLeft = cvQueryFrame(camera1);
+	imageRight = cvQueryFrame(camera2);//Posledovatelnoe schitovanie kadrov c kameri 1 i 2
+}
+
+void stereoGrabberStopCam()
+{
+	cvReleaseCapture(&camera1);
+	cvReleaseCapture(&camera2);//Osvobowdenie pamyati
+}
+
+void SCalibration()
+{
+	int nx = 10, ny = 7, cycle = 40, result1 = 0, result2 = 0; //Колличество углов по осям х и у, колличество циклов, результаты поиска углов
+	int n = nx*ny; //Колличество углов на шахмтной доске
+	int successes1 = 0, successes2 = 0;
+	int frame = 0, corners_count1 = 0, corners_count2 = 0, step = 0;
+	CvSize board_size = cvSize(nx, ny); //Размер доски
+	vector<CvPoint2D32f> found_points_coords1(n);
+	vector<CvPoint2D32f> found_points_coords2(n);//Массив координат точек найденных на шахматной доске
+
+	//Запуск веб-камер
+	cust->stereoGrabberGrabFrames();
+	cust->stereoGrabberInitFrames();
+	IplImage *frame1 = cust->imageLeft;
+	IplImage *frame2 = cust->imageRight; //Захват изображения с 1 и 2 камеры
+	IplImage *gr_frame1, *gr_frame2;
+
+	//Выделение памяти под хранилища
+
+	CvMat* image_points1 = cvCreateMat(cycle*n, 2, CV_32FC1);
+	CvMat* image_points2 = cvCreateMat(cycle*n, 2, CV_32FC1);
+	CvMat* object_points1 = cvCreateMat(cycle*n, 3, CV_32FC1);
+	CvMat* object_points2 = cvCreateMat(cycle*n, 3, CV_32FC1);
+	CvMat* point_counts1 = cvCreateMat(cycle, 1, CV_32SC1);
+	CvMat* point_counts2 = cvCreateMat(cycle, 1, CV_32SC1);
+	//Создание окон для отображения изображения получаемого с камер
+	cvNamedWindow("Camera 1", 1);
+	cvNamedWindow("Camera 2", 1);
+
+	while ((successes1>cycle) || (successes2>cycle))
+	{
+		//Ожидание 20 кадров доски с разных ракурсов для захвата изображения
+		if (frame++ % 20 == 0)
+		{
+			//Функция для поиска углов на доске(с точностью пикселя), со встроенной нормализацией и поиском черных квадратов для увеличения точности калибровки
+			result1 = cvFindChessboardCorners(frame1,
+				board_size,
+				&found_points_coords1[0],
+				NULL,
+				CV_CALIB_CB_NORMALIZE_IMAGE | CV_CALIB_CB_FILTER_QUADS);
+			result2 = cvFindChessboardCorners(frame2,
+				board_size,
+				&found_points_coords2[0],
+				NULL,
+				CV_CALIB_CB_NORMALIZE_IMAGE | CV_CALIB_CB_FILTER_QUADS);
+
+			//Конвертация изображений в градации серого
+			cvConvertImage(frame1, gr_frame1, CV_8S);
+			cvConvertImage(frame1, gr_frame1, CV_8S);
+
+			//Функция для поиска углов на доске(с точностью субпикселя), со встроенной нормализацией и поиском черных квадратов для увеличения точности калибровки
+			cvFindCornerSubPix(gr_frame1,
+				&found_points_coords1[0],
+				corners_count1,
+				cvSize(11, 11),
+				cvSize(-1, -1),
+				cvTermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 100, 0.01));
+
+			cvFindCornerSubPix(gr_frame2,
+				&found_points_coords2[0],
+				corners_count2,
+				cvSize(11, 11),
+				cvSize(-1, -1),
+				cvTermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 100, 0.01));
+
+			//Отрисовка найденых углов
+			cvDrawChessboardCorners(frame1,
+				board_size,
+				&found_points_coords1[0],
+				corners_count1,
+				result1);
+			cvDrawChessboardCorners(frame2,
+				board_size,
+				&found_points_coords2[0],
+				corners_count2,
+				result2);
+
+			//Отображение изображения
+			cvShowImage("Camera1", frame1);
+			cvShowImage("Camera2", frame2);
+
+
+			//Добавление хорошего(у котрого задетектированы все углы) представления доски в коллекцию
+			if (corners_count1 == n || corners_count2 == n)
+			if (corners_count1 == n)
+			{
+				step = successes1*n;
+				for (int i = step, j = 0; j < n; ++i, ++j)
+				{
+					CV_MAT_ELEM(*image_points1, float, i, 0) = found_points_coords1[j].x;
+					CV_MAT_ELEM(*image_points1, float, i, 1) = found_points_coords1[j].y;
+					CV_MAT_ELEM(*object_points1, float, i, 0) = j / nx;
+					CV_MAT_ELEM(*object_points1, float, i, 1) = j%nx;
+					CV_MAT_ELEM(*object_points1, float, i, 1) = 0.0f;
+				}
+			}
+			if (corners_count2 == n)
+			{
+				step = successes2*n;
+				for (int i = step, j = 0; j < n; ++i, ++j)
+				{
+					CV_MAT_ELEM(*image_points2, float, i, 0) = found_points_coords2[j].x;
+					CV_MAT_ELEM(*image_points2, float, i, 1) = found_points_coords2[j].y;
+					CV_MAT_ELEM(*object_points2, float, i, 0) = j / nx;
+					CV_MAT_ELEM(*object_points2, float, i, 1) = j%nx;
+					CV_MAT_ELEM(*object_points2, float, i, 1) = 0.0f;
+				}
+				CV_MAT_ELEM(*point_counts1, int, successes1, 0) = n;
+				CV_MAT_ELEM(*point_counts2, int, successes2, 0) = n;
+				successes1++;
+				successes2++;
+			}
+		}
+
+		//Съемка следущей фотографии
+		cust->stereoGrabberGrabFrames();
+		frame1 = cust->imageLeft;
+		cvShowImage("Camera 1", frame1);
+		frame2 = cust->imageRight;
+		cvShowImage("Camera 1", frame2);
+
+		//Ожидание нажатия клавишы для завершения
+		if (cvWaitKey(15) == 27) break;
+	}
+
+	//Калибровка камеры
+	//cvCalibrateCamera2(
+	//	object_points1,
+	//	image_points1,
+	//	point_counts1,
+	//	cvGetSize(frame1),
+
+	//	);
+
+	//Завершение работы камер
+	cust->stereoGrabberStopCam();
+
 }
