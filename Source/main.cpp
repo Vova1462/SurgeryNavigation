800adf34604bf38a89ce2540fdbc25e01bc23173
Vladimir <Sencor@bk.ru>
2016-10-25 23:42:54 +0300

    Add normalization

--- Source/main.cpp
+++ Source/main.cpp
@@ -484,16 +484,16 @@ int main(int argc, char** argv)
 	const string postfix = ".png";
 	Vec3f coords_and_radius;
 	Rect roi1, roi2;
-	double depth=0, baseline=90, focal_length=4.1 , sencorsize=0.005, X=0, Y=0,cx=0,cy=0;
+	double depth=0, baseline=90, focal_length=4.1, sencor_elem_size=5.5, X=0, Y=0,cx=0,cy=0;
 	int source_of_image = 0;
 	double fps = atof(argv[1]);
 	
 	
 	//Инициализация камер
-	VideoCapture cap1(2);
+	VideoCapture cap1(0);
 	if (!cap1.isOpened())
 		return -1;
-	VideoCapture cap2(0);
+	VideoCapture cap2(2);
 	if (!cap2.isOpened())
 		return -2;
 
@@ -570,10 +570,6 @@ int main(int argc, char** argv)
 			frame1 = imread(img_prefix + "1_2" + postfix, 1);
 			frame2 = imread(img_prefix + "2_2" + postfix, 1);
 		}
-
-		//Исправление изображений
-		CreatROI(&frame1, &frame2, &roi1, &roi2, frame1.size(),&cx,&cy);
-
 		//Создания изображений
 		gframe1.create(480, 640, CV_8UC1);
 		gframe2.create(480, 640, CV_8UC1);
@@ -581,10 +577,27 @@ int main(int argc, char** argv)
 		//Конвертация изображений в градации серого
 		cvtColor(frame1, gframe1, CV_BGR2GRAY);
 		cvtColor(frame2, gframe2, CV_BGR2GRAY);
+
+		//Нормализация изображений
+		unsigned int bright_of_frame1 = 0, bright_of_frame2 = 0;
+		double difference_of_averadge_bright = 0;
+		for (int i = 0;i<640;i++)
+			for (int j = 0;j < 480;j++)
+			{
+				bright_of_frame1 += gframe1.at<uchar>(j, i);
+				bright_of_frame2 += gframe2.at<uchar>(j, i);
+			}
+		difference_of_averadge_bright = abs((bright_of_frame1 / (640 * 480)) - (bright_of_frame2 / (640 * 480)));
+		for (int i = 0; i < 640;i++)
+			for (int j = 0; j < 480;j++)
+				gframe1.at<uchar>(j, i) -= difference_of_averadge_bright;
+
+		//Исправление изображений
+		CreatROI(&gframe1, &gframe2, &roi1, &roi2, gframe1.size(),&cx,&cy);
 		
-		//Поиск маркеров
-		FindCircles(gframe1,parametrs_for_hough, &circles1);
-		FindCircles(gframe2, parametrs_for_hough, &circles2);
+		//Уменьшение шума на изображениях
+		medianBlur(gframe1, gframe1, 3);
+		medianBlur(gframe2, gframe2, 3);
 
 		//Задание параметров для поиска окружностей
 		parametrs_for_hough[0] = getTrackbarPos("Threshold", "Parameters for Hough");
@@ -593,7 +606,11 @@ int main(int argc, char** argv)
 		parametrs_for_hough[3] = getTrackbarPos("MinRad", "Parameters for Hough");
 		parametrs_for_hough[4] = getTrackbarPos("MaxRad", "Parameters for Hough");
 		parametrs_for_hough[5] = getTrackbarPos("Blured", "Parameters for Hough");
-		
+
+		//Поиск маркеров
+		FindCircles(gframe1,parametrs_for_hough, &circles1);
+		FindCircles(gframe2, parametrs_for_hough, &circles2);
+				
 		//Поиск карты неравенства
 		if (first_time_matching)
 			//Подбор параметров на одном изображении для наилучшего отображения карты глубины
@@ -601,14 +618,15 @@ int main(int argc, char** argv)
 			{
 				imshow("capture1", gframe1);
 				imshow("capture2", gframe2);
-				StereoMatch(gframe1, gframe2, &disp, parametrs_for_matching);
-				imshow("disparity", disp);
-
+				
 				parametrs_for_matching[0] = getTrackbarPos("setPreFilterCap", "Parameters for StereoMatching");
 				parametrs_for_matching[1] = getTrackbarPos("setBlockSize", "Parameters for StereoMatching");
 				parametrs_for_matching[2] = getTrackbarPos("setTextureThreshold", "Parameters for StereoMatching");
 				parametrs_for_matching[3] = getTrackbarPos("setNumDisparities", "Parameters for StereoMatching");
 				parametrs_for_matching[4] = getTrackbarPos("setUniquenessRatio", "Parameters for StereoMatching");
+				
+				StereoMatch(gframe1, gframe2, &disp, parametrs_for_matching);
+				imshow("disparity", disp);
 
 				char c = (char)waitKey(60);
 				if (c == 27)
@@ -624,7 +642,7 @@ int main(int argc, char** argv)
 		}
 
 		//Вычисление поля зрения камеры
-		double field_of_view = 2 * atan(sencorsize / 2 * focal_length);
+		double field_of_view = 2 * atan(sencor_elem_size / 2 * focal_length);
 
 		//Отображение окружностей на карте глубины, подсчет расстояния и отображение на изображении
 		for (size_t i = 0; i < circles1.size(); i++)
@@ -632,9 +650,11 @@ int main(int argc, char** argv)
 			coords_and_radius = circles1[i];
 			circle(disp, Point(coords_and_radius[0], coords_and_radius[1]), coords_and_radius[2], Scalar(0, 0, 255), 3, LINE_AA);
 			circle(disp, Point(coords_and_radius[0], coords_and_radius[1]), coords_and_radius[2], Scalar(0, 255, 0), 3, LINE_AA);
-			depth = ((baseline*focal_length) / (sencorsize*disp.at<uchar>(coords_and_radius[1], coords_and_radius[0])));
+			
+			depth = ((baseline*focal_length) / (sencor_elem_size*disp.at<uchar>(coords_and_radius[1], coords_and_radius[0])));
 			X= 2 * sin(field_of_view / 2)*depth / gframe1.cols*(coords_and_radius[0]-cx);
 			Y = 2 * sin(field_of_view / 2)*depth / gframe1.rows*(coords_and_radius[1] - cy);
+
 			putText(frame1, to_string(depth), Point(coords_and_radius[0], coords_and_radius[1]), 1, 1, Scalar(0, 0, 255));
 			putText(frame1, to_string(X), Point(coords_and_radius[0], coords_and_radius[1] + 15), 1, 1, Scalar(0, 0, 255));
 			putText(frame1, to_string(Y), Point(coords_and_radius[0], coords_and_radius[1]+30), 1, 1, Scalar(0, 0, 255));
